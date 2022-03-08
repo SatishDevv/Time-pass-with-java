061803aaa9ff2e5489ff1e42315a9dc73d184b65
Satishghode <88142229+Satishghode@users.noreply.github.com>
2022-03-08 07:28:15 -0800

    Add files via upload

---
+++ emoji1.java
@@ -0,0 +1,28 @@
+/*
+        ##print the following pattern using emoji
+
+
+     😘 😘 😘 😘 😘
+    😘 😘 😘 😘 😘
+   😘 😘 😘 😘 😘
+  😘 😘 😘 😘 😘
+ 😘 😘 😘 😘 😘
+
+
+*/
+
+public class emoji1  {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for (int i =1;i<=n; i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=1 ; j<=n; j++){
+                System.out.print(" 😘");
+            }
+            System.out.println();
+        }   
+    }
+}   
+
